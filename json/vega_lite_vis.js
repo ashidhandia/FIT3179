ee6284833786d6a00665c27dbf58f3dfcd1832cf
yournewgoodname <ashiii6603@gmail.com>
2024-10-18 12:44:23 +0800

    update

---
+++ json/vega_lite_vis.js
@@ -0,0 +1,10 @@
+var vg_1 = "vis.json";
+vegaEmbed("#bar_chart", vg_1).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "EarthOvershootDay.json";
+vegaEmbed("#vg1", vg_2, {"actions": false}).then(function(result){}).catch(console.error);
+
+var vg_2 = "vg2.json";
+vegaEmbed("#vg2", vg_2, {"actions": false}).then(function(result){}).catch(console.error);
